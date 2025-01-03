c168b561b0d400118cafc92259e106868acfad0b
Karangin Stepan <s.karangin@dbfhub.com>
2025-01-04 00:28:11 +1000

    feat: add user dto

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,1 +1,10 @@
 package dto
+
+import "gitlab.com/xamops/auth/internal/entity"
+
+type User struct {
+	Email          string      `json:"email"`
+	OrganizationID string      `json:"organization_id"`
+	IsActive       bool        `json:"is_active"`
+	Role           entity.Role `json:"role"`
+}
