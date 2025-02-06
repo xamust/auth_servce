d9cc240d9390297fd8023e647ad981c7589704e6
Karangin Stepan <s.karangin@dbfhub.com>
2025-02-06 23:35:44 +0300

    feat: add CORS (Cross-Origin Resource Sharing)  middleware

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,7 +8,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 
 		//if c.Request.Method == "OPTIONS" {
 		//	c.AbortWithStatus(http.StatusNoContent)
