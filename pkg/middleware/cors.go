099e5b8cf25854a702fbd7aacd5346fe806fe484
Karangin Stepan <s.karangin@dbfhub.com>
2025-02-07 15:27:16 +0300

    feat: upd CORS middleware

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://special.xamust.tech")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://service.xamust.tech")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
